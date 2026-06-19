cffaf4be438cf136ae01bbc45a82fe5211702762
Richard Knutz <richardknutzjr@hotmail.com>
2026-06-19 16:11:40 -0700

    balance(exp): set EXP_PARTY_GAP_NO_EXP=10 (no EXP if >10 levels below highest party member)

--- settings/default/map.lua
+++ settings/default/map.lua
@@ -69,7 +69,7 @@ xi.settings.map =
     -- A party member's experience points are nullified if the level difference with the highest-level party member exceeds this value.
     -- When set to 0, there is no nullification of EXP regardless of how wide the gap is between party members.
     -- When set to 10, if you are level 65 or below in a party with a level 75, you will receive no EXP.
-    EXP_PARTY_GAP_NO_EXP = 0,
+    EXP_PARTY_GAP_NO_EXP = 10,
 
     -- Capacity Point Settings
     CAPACITY_RATE = 1.0,
