a49bea0ce85bc5f1736b57472040bdf547638f74
shigukahz <60148067+shigukahz@users.noreply.github.com>
2026-07-14 10:13:55 +0100

    Update trigger areas and player positions in Zone.lua
    Fixed the rebounce areas and fixed left tunnel entrance not being restricted.

--- scripts/zones/Abdhaljs_Isle-Purgonorgo/Zone.lua
+++ scripts/zones/Abdhaljs_Isle-Purgonorgo/Zone.lua
@@ -8,7 +8,7 @@ zoneObject.onInitialize = function(zone)
       -- Invisible wall blocking the tunnel mouth near H-8.
     zone:registerCuboidTriggerArea(1, 499.0, -5.0, 462.0, 512.0, 2.0, 471.0) -- back right of !hub
         -- Invisible wall blocking the tunnel mouth near H-8.
-    zone:registerCuboidTriggerArea(2, 609.38, -3.0, 617.94, 461.65, 2.0, 469.12) -- back left of hub
+    zone:registerCuboidTriggerArea(2, 618.5, -5.0, 454.0, 624.0, 2.0, 464.0) -- back left of hub
 end
 
 zoneObject.onZoneIn = function(player, prevZone)
@@ -28,10 +28,10 @@ end
 
 zoneObject.onTriggerAreaEnter = function(player, triggerArea)
 	if triggerArea:getTriggerAreaID() == 1 then
-        player:setPos(514.0, -2.5, 459.0, 221)
+        layer:setPos(517.57, -3.4, 475.83, 226)
         player:printToPlayer('This area is off limits.')
 	elseif triggerArea:getTriggerAreaID() == 2 then
-        player:setPos(608.07, -2.74, 470.0, 156)
+        player:setPos(609.7, -2.5, 470.0, 158)
         player:printToPlayer('This area is off limits.')
     end
 end
